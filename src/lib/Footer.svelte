d488bf88b8d82978e749cb69757da619de97c9b4
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-02-09 14:57:39 +1300

    header and footer
    customised header and customised footer using bulma

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,12 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>&copy; Craighead Diocesan School 2024</p>
+  </div>
+</footer>
+<!-- 
+<style>
+  footer {
+    background-color: rgb(151, 151, 234);
+    font-family: 'Times New Roman', Times, serif;
+  }
+</style> -->
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,13 @@
-<header>
-  <h1 class="title">Welcome</h1>
+<header class="hero is-link">
+  <div class="hero-body">
+    <h1 class="title">Welcome</h1>
+    <h2 class="subtitle">Andra is great</h2>
+  </div>
 </header>
+
+<!-- <style>
+  header {
+    background-color: rgb(205, 167, 236);
+    font-family: 'Times New Roman', Times, serif;
+  }
+</style> -->
