da02a8732484634ef54a8863865a15f13c4503a2
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-02-09 15:09:44 +1300

    pic
    i added a picture of a blueberry

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,8 @@
   <h2>SvelteKit</h2>
 
   <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+
+  <img src="/blueberry.webp" alt="blueberry" />
 </main>
 
 <Footer />
