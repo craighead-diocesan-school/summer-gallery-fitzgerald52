75c7a67f2bdf852614111a41b4593a7c4b33e777
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-02-12 12:17:51 +1300

    i added collums so that i can write about my summer activities

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -7,6 +7,27 @@
 <Header />
 <Nav />
 <main class="content section">
-  <p>i like blueberries i am loosing the plot my name is emma</p>
+  <h2 class="title">My summer activities</h2>
+  <div class="columns">
+    <div class="column">
+      <h3>Summer activity 1</h3>
+      <p>fjdshgviojvafpsjvodjv</p>
+    </div>
+
+    <div class="column">
+      <h3>Summer activity 2</h3>
+      <p>fjdshgviojvafpsjvodjv</p>
+    </div>
+    <div class="column">
+      <h3>Summer activity 3</h3>
+      <p>fjdshgviojvafpsjvodjv</p>
+    </div>
+  </div>
 </main>
 <Footer />
+
+<style>
+  h2 {
+    text-align: center;
+  }
+</style>
