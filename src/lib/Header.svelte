297e22f8fd0fbc8ca6e120549ce44e1b6be59e40
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-02-12 10:56:49 +1300

    i styled my header using hero on bulma

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,13 +1,13 @@
-<header class="hero is-link">
-  <div class="ihero-body">
+<header class="hero is-primary">
+  <div class="hero-body">
     <h1 class="title">Welcome</h1>
     <h2 class="subtitle">Andra is great</h2>
   </div>
 </header>
 
-<!-- <style>
-  header {
-    background-color: rgb(205, 167, 236);
-    font-family: 'Times New Roman', Times, serif;
+<style>
+  .title {
+    font-weight: bold;
+    font-size: xx-large;
   }
-</style> -->
+</style>
