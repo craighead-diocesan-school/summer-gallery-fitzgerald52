8b3137c3e72e26f2c43b21e28364996cdcf1531a
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-02-15 21:01:14 +1300

    i added a Image component with source and caption props

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -2,6 +2,7 @@
   import Header from '$lib/Header.svelte'
   import Footer from '$lib/Footer.svelte'
   import Nav from '$lib/Nav.svelte'
+  import Image from '../../lib/Image.svelte'
 </script>
 
 <Header title="About me " subtitle="I am cool" />
@@ -12,15 +13,18 @@
     <div class="column">
       <h3>Summer activity 1</h3>
       <p>I went to lake benmore over the holidays with my grandparents, my mum, and my sister where i went water skiing and biscuiting.</p>
+      <Image src="skiing.jpg" alt="me water skiing " caption="Me water skiing "></Image>
     </div>
 
     <div class="column">
       <h3>Summer activity 2</h3>
       <p>over new years i stated with my friend in twizel where we went shopping and biscuiting</p>
+      <Image src="friends.jpg" alt="four girls" caption="me and my friends at lake twizel"></Image>
     </div>
     <div class="column">
       <h3>Summer activity 3</h3>
       <p>i went to lake hood with andra on a scorching hot day where we almost died of heat but we could not swim because the lake was gross</p>
+      <Image src="lakehood.jpg" alt="picture of lake " caption="lake hood"></Image>
     </div>
   </div>
 </main>
