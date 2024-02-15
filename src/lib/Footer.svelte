b7a962d43df07b010432014e09a3f5c276dd5bdf
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-02-15 20:40:48 +1300

    i added a footer with author and date props

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,11 @@
+<script>
+  export let Author = 'Emma Fitzgerald'
+  export let date = '2024'
+  export let organization = 'Craighead Diocesan School'
+</script>
+
 <footer class="footer">
   <div class="content has-text-centered">
-    <p>&copy; Craighead Diocesan School 2024</p>
+    <p>{Author} {date} {organization}</p>
   </div>
 </footer>
